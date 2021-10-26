d4d38614ef89f2c3f94ca96bea1eb0d55bd409ef
ecastillo462 <erickcastillo123451@gmail.com>
2021-10-25 21:33:23 -0600

    Clase 18 Octubre

---
+++ Factura2021_1400/Factura2021_1400/Modelos/DAO/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+namespace Factura2021_1400.Modelos.DAO
+{
+    public class Conexion
+    {
+        protected SqlConnection MiConexion = new SqlConnection(ConfigurationManager.ConnectionStrings["FacturaConexion"].ConnectionString);
+    }
+}
---
+++ Factura2021_1400/Factura2021_1400/Modelos/DAO/UsuarioDAO.cs
@@ -0,0 +1,38 @@
+﻿using Factura2021_1400.Modelos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factura2021_1400.Modelos.DAO
+{
+    public class UsuarioDAO: Conexion
+    {
+        SqlCommand comando = new SqlCommand();
+        public bool ValidarUsuario(Usuario user)
+        {
+            bool valido = false;
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append("SELECT 1 FROM USUARIO WHERE EMAIL = @Email AND CLAVE =@Clave;");
+                comando.Connection = MiConexion;
+                MiConexion.Open();
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = sql.ToString();
+                comando.Parameters.Add("@Email", SqlDbType.NVarChar, 50).Value = user.Email;
+                comando.Parameters.Add("@Clave", SqlDbType.NVarChar, 100).Value = user.Clave;
+                valido = Convert.ToBoolean(comando.ExecuteScalar());
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return valido;
+        }
+    }
+}
